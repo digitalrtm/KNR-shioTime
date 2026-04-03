5df264785f822f5844582299e32f6ca18a616492
Analog_r_ <136825695+digitalrtm@users.noreply.github.com>
2026-04-03 22:49:07 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+//DB一覧(Array)
+const dbList = {
+    'サンプルデータ': './dia_2604/2604-0403.json',
+}
+
+//通知更新周期
+const alertRefreshDelay = 1000;
+
+//24時超過時刻の表示を切る
+const display24cut = true;
+
+//メインのタイトル
+const mainTitle = 'バーカ';
